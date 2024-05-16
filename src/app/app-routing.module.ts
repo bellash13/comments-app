474375ce943e25ae3a226f5c315d0c906c08fe07
bellash13 <theobellash@gmail.com>
2024-05-16 12:49:41 +0200

    UI styles and English UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CommentFormComponent } from './components/comment-form/comment-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CommentFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
